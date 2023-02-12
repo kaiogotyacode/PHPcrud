22628bd2411506d898d8dcbe763399865ff94c84
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-02-12 11:35:42 -0300

    Preparing project ~ Adding bootstrap files[css/js]

---
+++ index.php
@@ -0,0 +1,15 @@
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+    <title> PHP CRUD </title>
+</head>
+<body>
+    <div class="container">
+        <h1 class="display-1 text-center"> Welcome </h1>
+    </div>
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
